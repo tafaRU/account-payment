7df85c56a141c266159ee55df207f9e3283d1f5b
Jay Vora(SerpentCS) <jay.vora@serpentcs.com>
2016-11-17 17:27:33 +0530

    [ADD] Added account_invoice_under_payment module

---
+++ account_invoice_under_payment/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# --------------------------------------------------------------------------
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2016-Today Serpent Consulting Services PVT. LTD.
+#    (<http://www.serpentcs.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+# ---------------------------------------------------------------------------
+
+{
+    "name": "Payment Order Under Payment",
+    "summary": "Payment Order Under Payment",
+    "version": "8.0.1.0.0",
+    "category": "Accounting",
+    'website': 'http://www.serpentcs.com',
+    "author": """Serpent Consulting Services Pvt. Ltd.,
+                Agile Business Group,
+                Odoo Community Association (OCA)""",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account_payment",
+    ],
+    "data": [
+        "views/payment_line_views.xml",
+        "views/move_line_views.xml",
+        "views/invoice_view.xml",
+    ],
+    "installable": True,
+}
---
+++ account_invoice_under_payment/models/order_create.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+# --------------------------------------------------------------------------
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2016-Today Serpent Consulting Services PVT. LTD.
+#    (<http://www.serpentcs.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+# ---------------------------------------------------------------------------
+
+import time
+from lxml import etree
+
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+
+
+class payment_order_create(osv.osv_memory):
+
+    _inherit = 'payment.order.create'
+
+    def search_entries(self, cr, uid, ids, context=None):
+        line_obj = self.pool.get('account.move.line')
+        mod_obj = self.pool.get('ir.model.data')
+        if context is None:
+            context = {}
+        data = self.browse(cr, uid, ids, context=context)[0]
+        search_due_date = data.duedate
+        domain = [('under_payment', '=', False), ('reconcile_id', '=', False), ('account_id.type',
+                                                                                '=', 'payable'), ('credit', '>', 0), ('account_id.reconcile', '=', True)]
+        domain = domain + \
+            ['|', ('date_maturity', '<=', search_due_date),
+             ('date_maturity', '=', False)]
+        line_ids = line_obj.search(cr, uid, domain, context=context)
+        context = dict(context, line_ids=line_ids)
+        model_data_ids = mod_obj.search(cr, uid, [('model', '=', 'ir.ui.view'), (
+            'name', '=', 'view_create_payment_order_lines')], context=context)
+        resource_id = mod_obj.read(cr, uid, model_data_ids, fields=[
+                                   'res_id'], context=context)[0]['res_id']
+        return {'name': _('Entry Lines'),
+                'context': context,
+                'view_type': 'form',
+                'view_mode': 'form',
+                'res_model': 'payment.order.create',
+                'views': [(resource_id, 'form')],
+                'type': 'ir.actions.act_window',
+                'target': 'new',
+                }
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
